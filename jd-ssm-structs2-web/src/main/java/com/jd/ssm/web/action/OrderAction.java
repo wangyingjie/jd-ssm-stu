3a57b34be0f10f20b546cbd468c62f44120d2a63
wangyingjie5 <wangyingjie1@jd.com>
2016-12-13 10:12:47 +0800

    structs2 一次web请求的里里外外

--- jd-ssm-structs2-web/src/main/java/com/jd/ssm/web/action/OrderAction.java
+++ jd-ssm-structs2-web/src/main/java/com/jd/ssm/web/action/OrderAction.java
@@ -5,9 +5,8 @@ import com.jd.ssm.web.base.BaseAction;
 
 /**
  * Created with IntelliJ IDEA.
- * User: ckm
- * Date: 15-10-29
- * Time: 下午10:29
+ * User: wangyingjie1
+ * Date: 16-12-12
  * To change this template use File | Settings | File Templates.
  */
 public class OrderAction extends BaseAction {
@@ -15,16 +14,16 @@ public class OrderAction extends BaseAction {
 
     public String save(){
 
-        System.out.println("index===============================================");
+        System.out.println("save===============================================");
 
-        return "success";
+        return "save";
     }
 
     public String list() throws Exception {
 
-        System.out.println("execute===============================================");
+        System.out.println("list===============================================");
 
-        return "success";
+        return "list";
     }
 
 }
